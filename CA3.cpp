c811b12a879ec7693f539dc0072efedc8f8cb730
mohammad6pari <mohammad6pari@gmail.com>
2023-06-03 13:33:57 +0330

    show and topology instruction added

--- CA3.cpp
+++ CA3.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <tuple>
 
 #define SEPERATOR   ' '
 #define CONTINUE    "continue"
 #define EXIT    "exit"
-
+#define TOPOLOGY    "topology"
+#define NODE_SEPERATOR  '-'
+#define SHOW    "show"
+#define MODIFY  "modify"
 using namespace std;
+
+int graphSize = 1000;
+vector<Node> graph(graphSize);
 
 struct Edge{
     int neighbor;
@@ -27,7 +34,7 @@ bool edgeExists(const vector<Edge>& neighbors, int neighbor) {
     return false;
 }
 
-void addEdge(vector<Node>& graph, int node1, int node2, int cost) {
+void addEdge(int node1, int node2, int cost) {
     if (edgeExists(graph[node1].neighbors, node2)) {
         // If the edge already exists, update the cost
         for (Edge& edge : graph[node1].neighbors) {
@@ -56,7 +63,7 @@ void addEdge(vector<Node>& graph, int node1, int node2, int cost) {
     }
 }
 
-void printGraph(const vector<Node>& graph) {
+void printGraph() {
     for (const Node& node : graph) {
         cout << "Node " << node.id << " Neighbors: ";
         for (const Edge& edge : node.neighbors) {
@@ -80,14 +87,97 @@ vector<string> getInput(){
     return parsedCommand;
 }
 
+tuple<int,int,int> parseTopologyCommand(const string& input){
+    istringstream iss(input);
+
+    string token;
+    int node1, node2, cost;
+
+    getline(iss, token, NODE_SEPERATOR);
+    node1 = stoi(token);
+
+    getline(iss, token, NODE_SEPERATOR);
+    node2 = stoi(token);
+
+    getline(iss, token, NODE_SEPERATOR);
+    cost = stoi(token);
+
+    return make_tuple(node1, node2, cost);
+
+}
+
+void printAdjacencyMatrix() {
+    int numNodes = graph.size();
+
+    vector<vector<int>> adjacencyMatrix(numNodes, vector<int>(numNodes, 0));
+
+    for (const Node& node : graph) {
+        int nodeId = node.id;
+        for (const Edge& edge : node.neighbors) {
+            int neighborId = edge.neighbor;
+            int cost = edge.cost;
+            adjacencyMatrix[nodeId][neighborId] = cost;
+            adjacencyMatrix[neighborId][nodeId] = cost;
+        }
+    }
+
+    for (const vector<int>& row : adjacencyMatrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
 int processInput(vector<string> parsedCommand){
     string command= (parsedCommand.size()>0)? parsedCommand[0] : CONTINUE;
-    if (command==CONTINUE){
+    if (command==TOPOLOGY){
+        graphSize= parsedCommand.size()+1;
+        graph.resize(graphSize);
+        graph.clear();
+        for (int i = 0; i < graphSize; i++) {
+            graph[i].id = i;
+        }
+
+        for(int i=1;i<parsedCommand.size();i++){
+            string currInp = parsedCommand[i];
+            int node1, node2, cost;
+            tie(node1, node2, cost) = parseTopologyCommand(currInp);
+
+            if (node1==node2){
+                cout << "wrong input. source and destination node are equal.\n";
+            } else{
+                addEdge(node1,node2,cost);
+                cout << "OK\n";
+            }
+        }
+        return 1;
+
+    } else if (command==SHOW){
+        printAdjacencyMatrix();
+        return 1;
+
+    } else if (command==MODIFY){
+        string currInp = parsedCommand[1];
+        int node1, node2, cost;
+        tie(node1, node2, cost) = parseTopologyCommand(currInp);
+
+        if (node1==node2){
+            cout << "ERROR\n";
+        } else{
+            addEdge(node1,node2,cost);
+            cout << "OK\n";
+        }
+        return 1;
+
+    } else if (command==CONTINUE){
         cout << "no input found\n";
         return 1;
+
     } else if (command==EXIT){
         cout << "exit\n";
         return 0;
+
     } else {
         cout << "invalid input, enter another input or \"help\" for see commands.\n";
         return 1;
@@ -97,37 +187,12 @@ int processInput(vector<string> parsedCommand){
 
 int main(){
 
-    int maxNodes = 1000;
-    vector<Node> graph(maxNodes);
-    for (int i = 0; i < maxNodes; i++) {
-        graph[i].id = i;
-    }
-
     int currAns=1;
     while(currAns==1){
     vector<string> currInput;
     currInput = getInput();
     currAns = processInput(currInput);
     }
-
-
-    // addEdge(graph, 0, 1, 2);
-    // addEdge(graph, 0, 3, 4);
-    // addEdge(graph, 1, 2, 3);
-    // addEdge(graph, 1, 3, 1);
-    // addEdge(graph, 2, 4, 5);
-    // addEdge(graph, 3, 4, 2);
-
-    // Print the neighbors of each node with their costs
-    // printGraph(graph);
-
-    // Update the cost of an existing edge
-    // addEdge(graph, 0, 1, 5);
-
-    // Print the updated graph
-    // cout << "Updated Graph:\n";
-    // printGraph(graph);
-
     return 0;
 
 }
